761115c061f3ae7c6a1cc782faee7539d51b36d6
saumy <saumyadivyanjalee529@gmail.com>
2025-07-23 10:22:30 +0530

    BOFactory comment

--- layered-architecture/src/main/java/com/example/layeredarchitecture/bo/custom/BOFactory.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/bo/custom/BOFactory.java
@@ -6,23 +6,23 @@ import com.example.layeredarchitecture.bo.custom.impl.PlaceOrderBOimpl;
 
 public class BOFactory {
     private static BOFactory boFactory;
-    private BOFactory() {}
+    private BOFactory() {}//constructor private,return type na,0bject create krnn ba ,private use krla nisa,but method share krnn one nisa,static use krnwa.
     public static BOFactory getInstance() {
-        return (boFactory == null) ? new BOFactory() : boFactory;
-    }
+        return (boFactory == null) ? new BOFactory() : boFactory;//bofactory one unoth eka gnn,null nm object ekk hadala return krnwa,tiye nm e instance ekm return krnwa
+    }//singleton tiye,factory design pattern walt use krnwa,object create krna eka hide krnwa
 
-    public enum BOTypes {
+    public enum BOTypes {//types define krnwa//enum item define krgnnwa
         CUSTOMER,
         ITEM,
         ORDER,
         PLACE_ORDER,
 
     }
-    public SuperBO getBO(BOTypes boType) {
+    public SuperBO getBO(BOTypes boType) {//method type eka anuwa related bo tika return krnwa
         switch (boType) {
             case CUSTOMER:
                 return new CustomerBOImpl();
-            case ITEM:
+            case ITEM://case eka item nm return krnn ItemBOImpl ekk.. e widiyt krnne
                 return new ItemBOimpl();
             case PLACE_ORDER:
                 return new PlaceOrderBOimpl();
--- layered-architecture/src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
@@ -43,7 +43,7 @@ public class ManageCustomersFormController {
     public TableView<CustomerTM> tblCustomers;
     public JFXButton btnAddNewCustomer;
 
-      CustomerBO customerBO = (CustomerBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.CUSTOMER);
+      CustomerBO customerBO = (CustomerBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.CUSTOMER);//factory design pattern
 
     public void initialize() {
         tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
